0d92189b7b218e071077e3df721ed71354c1d77a
Alexander Koskovich <zvnexus@outlook.com>
2020-11-23 23:22:02 -0700

    op8t: Add overlays for refresh rate and power profile.
    Change-Id: I73c6bfa7383457a2c99a2ea0b2c10440da679d97

--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-dot
 
+PRODUCT_PACKAGES += \
+    OnePlus8TFrameworks
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
